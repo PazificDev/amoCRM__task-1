de21ce461741a494ac0598cd19290994a2a426ea
Pazific <advolatum@yandex.ru>
2023-07-24 11:35:37 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <input placeholder="Seconds" type="text" />
+
+      <button>Start</button>
+
+      <br />
+      <br />
+
+      <span>hh:mm:ss</span>
+    </div>
+  );
+}
+
+export default App;
